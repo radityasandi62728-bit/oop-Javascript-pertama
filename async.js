c3a901a67d97270fef20087864ae29fc94aff707
Raditya <radityasandi62728@gmail.com>
2026-02-16 19:21:05 +0700

    belajar async JS

--- async.js
+++ async.js
@@ -1,17 +1,17 @@
 function step1() {
     return Promise.resolve("A")
 }
-function step2() {
+function step2(value) {
     return Promise.resolve(value + "B")
 }
-function step3() {
+function step3(value) {
     return Promise.resolve(value + "C")
 }
 
 async function  run() {
-    let v1 = await step1("A")
-    let v2 = await step2(v1 + "B")
-    let v3 = await step3(v2 + "C")
-    Displayer(v3)
+    let v1 = await step1()
+    let v2 = await step2(v1)
+    let v3 = await step3(v2)
+    console.log(v3)
 }
 run();
